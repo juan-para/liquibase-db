62203c7e582157778dfa4294459902327231b2fc
juan-para <juanpara86@gmail.com>
2023-02-15 22:45:59 -0300

    Initial commit

---
+++ src/main/java/com/liquibasedb/LiquibaseDatabaseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.liquibasedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDatabaseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDatabaseManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liquibasedb/LiquibaseDatabaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquibasedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDatabaseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
